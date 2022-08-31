507110874827d39e3cfa298326e23add0e8e0459
Alexander Klauber <alexander.klauber.aj1@is.naist.jp>
2022-08-31 12:52:50 +0900

    add probabilistic belief model class

--- models.py
+++ models.py
@@ -5,6 +5,7 @@ import torch.nn.functional as f
 import yaml
 
 from utils import helpers as h
+import encoder_decoder as ed
 
 
 
@@ -33,7 +34,6 @@ class NaiveModel(torch.nn.Module):
 
     def forward(self, x):
         x = self.policy(x)
-        x = 3*torch.tanh(x)
         return x
 
     def save_model(self, env_name):
@@ -60,13 +60,19 @@ class BeliefModel(torch.nn.Module):
             self.belief_gru = torch.nn.GRUCell(obs_dim + acs_dim, self.belief_dim)
         else:
             self.belief_gru = torch.nn.GRUCell(obs_dim, self.belief_dim)
+        
         self.belief_gru = self.belief_gru.double()
+
+        # self.decoder_fwd_1step = ed.get_decoder(
+        #     input_dim=self.belief_dim + acs_dim,
+        #     output_dim=obs_dim, 
+        #     decoder_hidden=config['decoder_hidden'],
+        #     nonlinearity= nn.ReLu())
         
     def forward(self, memory):   
         if self.prev_acs:
             x = torch.cat((memory['curr_ob'], memory['prev_ac']), dim=0)
         else:
-            print("!")
             x = memory['curr_ob']
         
         prev_belief = memory['prev_belief'].double()
@@ -76,12 +82,55 @@ class BeliefModel(torch.nn.Module):
 
         pol_in = belief.squeeze()
         acs = self.policy(pol_in)
-        acs = 3*torch.tanh(acs)
         return acs, belief
         
 
     def save_model(self, env_name):
         torch.save(self.state_dict(), os.path.join(self.model_dir, env_name))
+
+
+class ProbBeliefModel(torch.nn.Module):
+    def __init__(self, config, acs_dim, obs_dim):
+        super(ProbBeliefModel,self).__init__()
+        
+        self.hidden = config['actor_hidden_dim']
+        self.belief_dim = config['belief_dim']
+        self.prev_acs = config['prev_acs']
+        self.policy = torch.nn.Sequential(
+            nn.Linear(self.belief_dim + self.belief_var_dim, self.hidden),
+            nn.ReLU(),
+            nn.Linear(self.hidden, self.hidden),
+            nn.ReLU(),
+            nn.Linear(self.hidden, acs_dim)
+        )
+        
+        self.policy = self.policy.double()
+        if self.prev_acs:
+            self.belief_gru = torch.nn.GRUCell(obs_dim + acs_dim, self.belief_dim)
+        else:
+            self.belief_gru = torch.nn.GRUCell(obs_dim, self.belief_dim)
+        self.belief_gru = self.belief_gru.double()
+        self.variance_predictor = nn.Linear()
+        
+    def forward(self, memory):   
+        if self.prev_acs:
+            x = torch.cat((memory['curr_ob'], memory['prev_ac']), dim=0)
+        else:
+            x = memory['curr_ob']
+        
+        prev_belief = memory['prev_belief'].double()
+
+        #print(x.size())
+        belief = self.belief_gru(x.double(), prev_belief.detach())
+
+        pol_in = belief.squeeze()
+        acs = self.policy(pol_in)
+        return acs, belief
+        
+
+    def save_model(self, env_name):
+        torch.save(self.state_dict(), os.path.join(self.model_dir, env_name))
+
 
 
 
