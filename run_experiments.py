9da4b99c09aa3865b4335d26105c30b50da2f687
Alexander Klauber <alexander.klauber.aj1@is.naist.jp>
2022-09-14 15:38:22 +0900

    implement policy training

--- run_experiments.py
+++ run_experiments.py
@@ -8,7 +8,7 @@ import pickle
 def quick_test(env, configs):
     print(env)
     bc = BehaviorCloner(env, configs=configs)
-    bc.train_vae()
+    bc.train()
     result = bc.get_results()
     
 
@@ -20,7 +20,6 @@ if __name__ == "__main__":
     
     results = []
     configs = h.get_params("./configs/learning_params.yaml")
-    l_config=configs['learning_params']
     env = "InvertedPendulum-v2"
-    quick_test(env, l_config)
+    quick_test(env, configs)
     
